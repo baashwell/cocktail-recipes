2669f108e2cd790ab0efa4faf27efa895362cac9
Ben Ashwell <ben.ashwell@matrixbooking.com>
2023-06-28 12:35:26 +0100

    Initial deploy of a Springboot service

---
+++ src/main/java/uk/co/benashwell/cocktailrecipe/CocktailRecipeApplication.java
@@ -0,0 +1,13 @@
+package uk.co.benashwell.cocktailrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/benashwell/cocktailrecipe/CocktailRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.benashwell.cocktailrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
